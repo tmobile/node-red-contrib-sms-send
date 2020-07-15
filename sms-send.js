a7dceacd6c6da19682188c4fdbd317862243bb77
Hill <Eric.Hill40@T-Mobile.com>
2020-07-15 15:00:30 -0500

    Update

--- sms-send.js
+++ sms-send.js
@@ -3,7 +3,7 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
-            var arr = msg.payload.split(",");
+            var arr = msg.payload[0].split(",");
             var number = Number(arr[0]);
             var message = arr[1];
             var SerialPort = require("serialport").SerialPort;
@@ -18,12 +18,12 @@ module.exports = function(RED) {
                     console.log('results ' + results);
                 });
                 setTimeout(function(){ console.log("sleep(3)"); }, 3000);
-                serialPort.write(new Buffer("AT+CMGS=\""+number+"\"\r", 'ascii'), function(err,$
+                serialPort.write(new Buffer("AT+CMGS=\""+number+"\"\r", 'ascii'), function(err, results) {
                     console.log('err ' + err);
                     console.log('results ' + results);
                 });
                 setTimeout(function(){ console.log("sleep(3)"); }, 3000);
-                serialPort.write(new Buffer(message + "\r\n" + chr(26), 'ascii'), function(err,$
+                serialPort.write(new Buffer(message + "\r\n" + chr(26), 'ascii'), function(err, results) {
                     console.log('err ' + err);
                     console.log('results ' + results);
                 });
