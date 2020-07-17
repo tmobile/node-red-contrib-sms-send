6d91cb7b8a146e1eb10b2d864b2521265c795cb9
Ehill40 <eric.hill40@t-mobile.com>
2020-07-17 15:53:51 -0500

    Timing Fix + Logs

--- sms-send.js
+++ sms-send.js
@@ -4,25 +4,29 @@ module.exports = function(RED) {
         var node = this;
         node.on('input', function(msg) {
             var arr = msg.payload.toString().split(",");
-            console.log('ARR:'+arr);
             var number = arr[0];
-            console.log('NUMBER:'+number);
             var message = arr[1];
-            console.log('MESSAGE:'+message);
             var SerialPort = require("serialport");
             var serialPort = new SerialPort("/dev/ttyUSB3", {
                 baudRate: 115200
             });
             serialPort.on("open", function() {
-                console.log('open');
+                console.log('PORT OPEN');
                 serialPort.write(new Buffer.from("AT+CMGF=1\r", 'utf8'), function(err, results) {
-                    serialPort.write(new Buffer.from("AT+CMGS=\""+number+"\"\r", 'utf8'), function(err, results) {
-                        serialPort.write(new Buffer.from(message + "\r\n" + String.fromCharCode(26), 'utf8'), function(err, results) {
-                            serialPort.close(function (err){
-                                console.log('port closed', err);
-                            });
+                     console.log("SMS MODE: AT+CMGF=1\r");
+                    setTimeout(function(){
+                        serialPort.write(new Buffer.from("AT+CMGS=\""+number+"\"\r", 'utf8'), function(err, results) {
+                            console.log("SET RECIPIENT: AT+CMGS=\""+number+"\"\r");
+                            setTimeout(function(){
+                                serialPort.write(new Buffer.from(message + "\r\n" + String.fromCharCode(26), 'utf8'), function(err, results) {
+                                    console.log("SENT MESSAGE:"+message+"\r");
+                                    serialPort.close(function (err){
+                                    console.log('PORT CLOSED');
+                                    });
+                                });
+                            }, 3000);
                         });
-                    });
+                    }, 3000);
                 });
             });
             node.send(msg);
